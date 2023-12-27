2e291c4a0f054b976c69b143ba3ad90fa58ed977
RoshanJadhav28 <147482246+RoshanJadhav28@users.noreply.github.com>
2023-12-27 10:30:57 +0530

    Add files via upload

---
+++ Iteration2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int i = 0;
+    int count =0;
+
+    printf("Enter the number : \n");
+    scanf("%d",&count);
+
+    for(i = 1; i <= count; i++)
+    {
+        printf("jay Ganesh...\n");
+    }
+    return 0;
+}
---
+++ Iteration5.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+
+    int i =1;
+    int count = 0;
+    
+    printf("Enter number of times you want to display JAY GANESH ON screen\n");
+    scanf("%d",&count);
+
+    do 
+    {
+        printf("Jay Ganesh...\n");
+        i++;
+    }while(i <= count);
+
+    return 0;
+}
